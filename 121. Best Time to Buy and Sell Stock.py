3684f686cc0bf8eae15d42e27d7b587a7bb57749
Khalil Peguero <khalilpeguero@hotmail.com>
2024-07-18 23:57:10 -0400

    Create 121. Best Time to Buy and Sell Stock.py

---
+++ 121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,34 @@
+"""
+121. Best Time to Buy and Sell Stock
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+"""
+
+class Solution1: # Two Pointers
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) < 2:
+            return 0
+        left = 0
+        right = 1
+        max_profit = 0
+        while right < len(prices):
+            if prices[left] < prices[right]:
+                profit = prices[right] - prices[left]
+                max_profit = max(max_profit, profit)
+            else:
+                left = right
+            right += 1
+        return max_profit
+
+class Solution2: #Kadane's Algorithm
+    def maxProfit(self, prices: List[int]) -> int:
+        buy = prices[0]
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] < buy:
+                buy = prices[i]
+            elif prices[i] - buy > profit:
+                profit = prices[i] - buy
+        return profit
